37bcfde8343909e307112051b0b31575b335ae62
Sasha <136902560+SashaG-dev@users.noreply.github.com>
2023-10-03 13:06:31 -0500

    Small fix to remove unnused function from settings

--- src/features/settings/SettingsLayout/SettingsLayout.tsx
+++ src/features/settings/SettingsLayout/SettingsLayout.tsx
@@ -1,25 +1,15 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import AccountSettings from '../AccountSettings';
 import UserSettings from '../UserSettings';
 import DeleteAccount from '../DeleteAccount';
 import { ButtonStyled } from '../../../components/Button';
-import { deleteUserAccount } from '../../../api/apiAuth';
 import { SettingsLayoutStyled } from './styles';
 
 const SettingsLayout = () => {
   const [display, setDisplay] = useState<boolean>(false);
 
-  const navigate = useNavigate();
-
   const showDisplay = () => setDisplay(true);
   const hideDisplay = () => setDisplay(false);
-
-  const onClick = () => {
-    deleteUserAccount();
-    localStorage.removeItem('token');
-    navigate('/login', { replace: true });
-  };
 
   return (
     <SettingsLayoutStyled>
